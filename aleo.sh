58edf616bbb615b1b95830feaad7625335b29814
guttergainz <155899448+guttergainz@users.noreply.github.com>
2024-02-21 18:25:51 +0100

    Update aleo.sh

--- aleo.sh
+++ aleo.sh
@@ -9,6 +9,8 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source "$HOME/.cargo/env"
 git clone https://github.com/AleoHQ/snarkOS.git --depth 1
 cd snarkOS
+git fetch origin testnet3:testnet3
+git checkout testnet3
 ./build_ubuntu.sh
 cargo install --path .
 snarkos account new
