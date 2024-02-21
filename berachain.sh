aee23ec3cde26de72dbf09a73dbd84b2e0ef1696
guttergainz <155899448+guttergainz@users.noreply.github.com>
2024-02-21 14:15:22 +0100

    Update berachain.sh

--- berachain.sh
+++ berachain.sh
@@ -1,13 +1,29 @@
+set_go_path() {
+    # Check if Go binary exists
+    if command -v go &>/dev/null; then
+        # Check if the Go path is already set
+        if [[ ":$PATH:" != *":/usr/local/go/bin:"* ]]; then
+            # Add Go binary directory to PATH
+            export PATH=$PATH:/usr/local/go/bin
+            echo "Go binary directory added to PATH."
+        else
+            echo "Go binary directory already in PATH."
+        fi
+    else
+        echo "Go binary not found. Ensure Go is installed and properly configured."
+    fi
+}
+
 cd ~
 mkdir -p $1
 cd $1
 mkdir berachain
 cd berachain
-apt-get update && sudo apt-get upgrade -y
-apt-get install git make screen jq -y
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install git make screen jq -y
 wget https://golang.org/dl/go1.21.4.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.21.4.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
+sudo tar -C /usr/local -xzf go1.21.4.linux-amd64.tar.gz
+set_go_bash
 curl -L https://foundry.paradigm.xyz | bash
 source /root/.bashrc
 foundryup
