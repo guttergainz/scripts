bea703f6b91ce09bbbe89d9eeb960f80823e9fa4
guttergainz <155899448+guttergainz@users.noreply.github.com>
2024-02-28 11:38:44 +0100

    Update berachain.sh

--- berachain.sh
+++ berachain.sh
@@ -1,19 +1,3 @@
-set_go_path() {
-    # Check if Go binary exists
-    if command -v go &>/dev/null; then
-        # Check if the Go path is already set
-        if [[ ":$PATH:" != *":/usr/local/go/bin:"* ]]; then
-            # Add Go binary directory to PATH
-            export PATH=$PATH:/usr/local/go/bin
-            echo "Go binary directory added to PATH."
-        else
-            echo "Go binary directory already in PATH."
-        fi
-    else
-        echo "Go binary not found. Ensure Go is installed and properly configured."
-    fi
-}
-
 cd ~
 mkdir -p $1
 cd $1
@@ -25,6 +9,7 @@ wget https://golang.org/dl/go1.21.4.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.21.4.linux-amd64.tar.gz
 set_go_bash
 curl -L https://foundry.paradigm.xyz | bash
+export PATH=$PATH:/usr/local/go/bin
 source /root/.bashrc
 foundryup
 git clone https://github.com/berachain/polaris
