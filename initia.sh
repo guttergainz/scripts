f03f5fe76a930e59c24be49e7cde1803c0f6f253
guttergainz <155899448+guttergainz@users.noreply.github.com>
2024-05-16 11:45:35 +0200

    Update initia.sh

--- initia.sh
+++ initia.sh
@@ -3,12 +3,12 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install curl git jq build-essential gcc unzip wget lz4 -y
 cd $HOME && \
 ver="1.22.0" && \
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
-sudo rm -rf /usr/local/go && \
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
-rm "go$ver.linux-amd64.tar.gz" && \
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile && \
-source $HOME/.bash_profile && \
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
+source $HOME/.bash_profile
 go version
 git clone https://github.com/initia-labs/initia.git
 cd initia
