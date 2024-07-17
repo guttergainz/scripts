ec435c16713a3afb9face82972fb0506822184e4
guttergainz <155899448+guttergainz@users.noreply.github.com>
2024-07-17 08:35:11 +0200

    Update hyperliquid.sh

--- hyperliquid.sh
+++ hyperliquid.sh
@@ -1,5 +1,11 @@
 ufw allow 8000
+sudo ufw allow ssh
 ufw allow 9000
+sudo ufw enable
+sudo echo 'deb http://security.ubuntu.com/ubuntu jammy-security main' >> nano /etc/apt/sources.list
+sudo apt update
+sudo apt upgrade
+sudo apt install libc6
 sudo adduser hyperliquid
 sudo usermod -aG sudo hyperliquid
 su - hyperliquid
@@ -7,3 +13,4 @@ curl https://binaries.hyperliquid.xyz/Testnet/initial_peers.json > ~/initial_pee
 echo '{"chain": "Testnet"}' > ~/visor.json
 curl https://binaries.hyperliquid.xyz/Testnet/non_validator_config.json > ~/non_validator_config.json
 curl https://binaries.hyperliquid.xyz/Testnet/hl-visor > ~/hl-visor && chmod a+x ~/hl-visor
+~/hl-visor
