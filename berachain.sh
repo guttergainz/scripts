e914d2b8efe8941b137efba5f350d61e1f61771d
guttergainz <155899448+guttergainz@users.noreply.github.com>
2024-08-27 17:57:39 +0200

    Update berachain.sh

--- berachain.sh
+++ berachain.sh
@@ -7,7 +7,7 @@ sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install git make screen jq -y
 wget https://golang.org/dl/go1.21.4.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.21.4.linux-amd64.tar.gz
-set_go_bash
+sudo apt-get install golang jq -y
 curl -L https://foundry.paradigm.xyz | bash
 export PATH=$PATH:/usr/local/go/bin
 source /root/.bashrc
